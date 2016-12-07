ac7bdf850d429bce26708eb0f56ddb3234158860
ianfitzpatrick1 <ianfitz@gmail.com>
2016-12-06 22:38:55 -0600

    Image compositing with arbitrary # messages + word wrap is working!

--- draw_text.py
+++ draw_text.py
@@ -1,5 +1,7 @@
 import os
+from textwrap import wrap
 from PIL import Image, ImageDraw, ImageFont
+
 
 # DEV
 BOTDIR = os.getcwd() + '/'
@@ -7,12 +9,12 @@ BOTDIR = os.getcwd() + '/'
 sprite_map_rows = [ ('W', 0, 22, 22),
                     ('@', 22, 44, 20),
                     ('AMVXYZmw%', 44, 66, 14),
-                    ('CDGHKNOPQRU#', 66, 88, 12),
-                    ('BEFLTSabcdeghknopquvy234567890?$^&+', 88, 110, 10),
-                    ('Jsxz/"', 110, 132, 8),
-                    ('1*', 132, 154, 6),
-                    ('frt()-;[]', 154, 176, 4),
-                    ("ijlI.!':", 176, 198, 2)
+                    ('CDGHKNOPQRU#_', 66, 88, 12),
+                    ('BEFLSTabcdeghnopquvyk234567890?$^&+=', 88, 110, 10),
+                    ('Jsxz/"<>\\', 110, 132, 8),
+                    ('1*{}', 132, 154, 6),
+                    ('frt()-;[], ', 154, 176, 4),
+                    ("ijlI.!':|", 176, 198, 2)
                     ] 
 
 
@@ -35,7 +37,7 @@ def build_sprite_map(chars, top, bottom, width):
 
     return sprite_map
 
-def build_sprite_sheet(sprite_map, sprite_img):
+def build_sprite_dict(sprite_map, sprite_img):
     """
     Build a dictionary of the actual sprites
 
@@ -46,27 +48,75 @@ def build_sprite_sheet(sprite_map, sprite_img):
 
     """
 
-    sprite_sheet = {}
+    sprite_dict = {}
 
     for char, pos in sprite_map.items():
         img = sprite_img.copy()
         img = img.crop(box=pos)
-        sprite_sheet[char] = img
+        sprite_dict[char] = img
 
-    return sprite_sheet
+    return sprite_dict
 
-def print_sprites(text, x, y, sprite_sheet, bg_img, char_width=10, kerning=2):
+def composite_msgs(msgs, bg_img, x, y, sprite_dict, char_height=22, kerning=2, line_spacing=5, msg_margin_bottom=20, wrap_width=200, bullet=None):
+    """
+    Composites a list of of messages to a background image, returning a new copy of
+    the composite.
 
-    for char in text:
+    Takes:
 
-        # Print character unless it's a space
-        if char != ' ':
-            bg_img.paste(sprite_sheet[char], (x, y))
+        Required
+        - msgs: A list of messages to composite
+        - bg_img: The background image to composite text onto
+        - x, y: Upper left corner starting coordinates
+        - sprite_dict: A dictioanry of sprite PIL image objects (the hacked together win 3.1 font)
+    
+        Optional
+        - char_height: height of characters in font
+        - kerning: Amount of space between characters
+        - line_spacing: Space between lines in multiple line message
+        - msg_margin_bottom: Space between messages
+        - wrap_width: maximum width of message 
+        - Bullet: Autuomatically add bullet text to beginning of each message
 
-        x = x + char_width + kerning
+    """
+
+    new_bg_img = bg_img.copy()
+    x_pos, x_indent, y_pos = x, x, y
+    for msg in msgs:
+        
+        # Composite bullet before message
+        if bullet:
+            indent_length = 0
+            for char in bullet:
+                new_bg_img.paste(sprite_dict[char], (x_pos, y_pos))
+                x_pos = x_pos + sprite_dict[char].width + kerning
+                indent_length += sprite_dict[char].width + kerning
+
+            x_indent = x + indent_length
+
+        # Split message into lines
+        msg = wrap(msg, wrap_width)
+
+        for line in msg:
+            for char in line:
+                new_bg_img.paste(sprite_dict[char], (x_pos, y_pos))
+                x_pos = x_pos + sprite_dict[char].width + kerning
+
+            # New Line, reset X and increment Y
+            x_pos = x_indent
+            y_pos = y_pos + char_height + line_spacing
+
+        # Reset x-pos and x-indent after each message
+        x_pos, x_indent = x, x
+        y_pos = y_pos + msg_margin_bottom
+
+    return new_bg_img
 
 
 def init_chars():
+    """
+    Build sprite dict and store in memory on program run
+    """
     sprite_img = Image.open("%sspritesheet.png" % BOTDIR).convert('RGBA')
 
     sprite_map = {}
@@ -74,15 +124,19 @@ def init_chars():
         result = build_sprite_map(row[0], row[1], row[2], row[3])
         sprite_map = dict( sprite_map.items() + result.items() )
 
-    sprite_sheet = build_sprite_sheet(sprite_map, sprite_img)
-    return sprite_sheet
+    sprite_dict = build_sprite_dict(sprite_map, sprite_img)
+    return sprite_dict
 
 # Run at startup
 chars = init_chars()
 
 # Running in shell for now
-# print_sprites('W', 500, 400, sprite_sheet, background)
-# background.show()
+# from draw_text import *
+# background = Image.open("%stemplates/accessible.png" % BOTDIR).convert('RGBA')
+# msgs = ["Large fonts, high-contrast color schemes, large mouse pointers, and pointer trails make your screen easier to read.", "FilterKeys, StickyKeys and MouseKeys make your keyboard and mouse pointer easier to control.", "Ease your sense of existential dread with our Fun Color Themes"]
+# result = composite_msgs(msgs, background, 500, 380, chars, wrap_width=57, bullet="-    ")
+# result.show()
+
 
 
 
